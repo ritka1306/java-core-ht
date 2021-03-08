821163153b890f7c8b0801e2b2ece629a6e89c7b
Ritik <2628908Fdhjh>
2021-03-08 14:24:49 +0300

    Add homework 2

---
+++ src/hometask02/Task4.java
@@ -0,0 +1,12 @@
+package hometask02;
+
+public class Task4 {
+
+    /**
+     * It prints the text "It works correctly" to the standard output
+     */
+
+    public static void main(String[] args) {
+        System.out.println("It works correctly");
+    }
+}
---
+++ src/hometask02/Task6.java
@@ -0,0 +1,7 @@
+package hometask02;
+
+public class Task6 {
+    public static void main(String[] args) {
+        System.out.println("IT College!");
+    }
+}

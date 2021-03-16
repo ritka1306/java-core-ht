3f7e98f5f23080182f118057b09e0d3ae9e28584
Ritik <2628908Fdhjh>
2021-03-16 18:02:36 +0300

    correct Task1

--- src/hometask08/Task1.java
+++ src/hometask08/Task1.java
@@ -7,10 +7,11 @@ public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         LocalDate date = LocalDate.now();
+        int nowDayOfYear = date.getDayOfYear();
         System.out.println("Текущая дата: " + date);
-        System.out.print("Введите количество дней с начала года: ");
-        int x = scanner.nextInt();
-        LocalDate y = date.plusDays(x);
-        System.out.println("Дата после прибавления дней: " + y);
+        System.out.println("Сегодняшний день от начала года: " + nowDayOfYear);
+        LocalDate a = date.plusDays(nowDayOfYear);
+        System.out.println("Дата после прибавления дней: " + a);
     }
 }
+
